9226ea4bdde8cb47079bbb9128ccb24855db67f1
sweety <smehta1289@gmail.com>
2013-07-27 22:19:02 +0530

    calender for date

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmRc213::Application.config.secret_token = '4b5163bf9182c678fcaa070e930796ae25c9587225de812657f3af0bd5fc3c6950d45e0b24b2e09efbeed324c03d2af1a41e4bd64a1f76c4e5db9f4a8411e515'
